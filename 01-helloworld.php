aaae4d75abab49d63c654ed76061dfcbbaa52bb2
benbakelaar <benbakelaar@gmail.com>
2017-04-11 21:54:33 -0400

    Update 01-helloworld.php

--- 01-helloworld.php
+++ 01-helloworld.php
@@ -1,5 +1,11 @@
 <?php
 
-phpinfo();
+//inside of PHP tags, you can still output HTML
+echo '<h1>Hello, world.</h1>';
 
+//you can use double quotes instead of single quotes
+echo "<h1>Hello, world 2.</h1>";
 ?>
+
+<!-- you can also write HTML inside a .PHP file but OUTSIDE of the PHP tags -->
+<h1>Hello, world 3.</h1>";
